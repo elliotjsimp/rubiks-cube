9fa82c2d4cf2d16a929622ba415af8e419602693
Elliot Simpson <elliotsimpson@uvic.ca>
2025-12-19 20:03:17 -0800

    Minor changes

--- src/cube.ts
+++ src/cube.ts
@@ -2,9 +2,9 @@ import { Cubie } from './cubie';
 import type { Vec3, Face, Color, FaceColors } from './cubie';
 
 export class Cube {
-    // A hashmap that describes the relationship from
+    // A map that describes the relationship from
     // axis to position to FaceColors for a Cubie on a solved Cube.
-    static readonly FACE_MAP: { [key: number]: { [key: string]: { face: Face, color: Color } } } = {
+    static readonly FACE_COLOR_SOLVED_MAP: { [key: number]: { [key: string]: { face: Face, color: Color } } } = {
         0: { // x axis
           "-1": { face: 'left',  color: 'orange' },
            "1": { face: 'right', color: 'red'    }
@@ -21,7 +21,6 @@ export class Cube {
 
     public cubies: Cubie[];
     public solvedState: Cubie[];
-
 
     public constructor() {
         this.cubies = [];
@@ -47,14 +46,14 @@ export class Cube {
                     
                     // Determine faceColors
                     for (let axis = 0; axis < 3; axis++) {
-                        const mapping = Cube.FACE_MAP[axis]?.[position[axis]];
+                        const mapping = Cube.FACE_COLOR_SOLVED_MAP[axis]?.[position[axis]];
                         if (mapping) {
                             faceColors[mapping.face as Face] = mapping.color as Color;
                         }
 
                     }
 
-                    // Could assert expected length of FaceColors makes sense based on position.
+                    // Note: Could assert expected length of FaceColors makes sense based on position here.
 
                     // Orientation for every Cubie in a solved Cube is [0, 0, 0]
                     // Create both array's with different references.
