b3d23fa0f65c65b455cbefb29ce4770f68c93aac
Elliot Simpson <elliotsimpson@uvic.ca>
2025-12-20 21:40:45 -0800

    Update test-print-cube.ts, note moves are functional

--- test-print-cube.ts
+++ test-print-cube.ts
@@ -1,5 +1,21 @@
 import { Cube } from './src/cube.ts';
 import { printCube } from './src/print-cube.ts';
+import * as move from './src/move.ts';
 
 const cube = new Cube();
+console.log("Solved Cube:\n");
+printCube(cube);
+
+move.doMove(cube, 'U');
+console.log("\n\nCube after move U:\n");
+printCube(cube);
+
+move.doMove(cube, 'R');
+console.log("\n\nCube after move R:\n");
+printCube(cube);
+
+move.doMove(cube, 'R_p');
+move.doMove(cube, 'U_p');
+
+console.log("\n\nCube after undoing those moves, should be solved:\n");
 printCube(cube);
